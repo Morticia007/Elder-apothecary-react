d328d43ba46e0123635f5069bf7b1af8dfd332db
Morticia007 <jacquelyn07@mac.com>
2021-02-07 22:11:43 -0600

    Setting up Elder Apothecary with React-app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+
+function App() {
+  return (
+    <div>
+      <h1>Hello Manny </h1>
+        <p> Building this is definitely going to force me to <strong>read</strong> and learn!!!</p>
+    </div>
+  );
+}
+
+
+
+export default App;
