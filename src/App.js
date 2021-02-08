3d727ddd1ff82548ee4e9320ef52ba052d033a2d
Morticia007 <jacquelyn07@mac.com>
2021-02-08 00:12:42 -0600

    commit on main

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
-import React, {Component}from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
 
 class App extends Component {
   render() {
-    return(
-      <div className= 'App'>
+    return (
+      <div className='App'>
         <Navbar dark color='success'>
           <div className='container'>
-            <NavbarBrand href='/'>Elder Apothecary <br></br><img 
-            src='/assets/images/ElderApothecary_v1light.png'
-            
-            height='30'
-            width='30'
-            alt='ElderApothecary Logo' />
+            <NavbarBrand className='mr-auto' href='/'>
+              Elder Apothecary <br></br>
+              <img
+                src='/assets/images/ElderApothecary_v1light.png'
+                height='30'
+                width='30'
+                alt='ElderApothecary Logo'
+              />
             </NavbarBrand>
-          
           </div>
         </Navbar>
       </div>
     );
   }
 }
-
-
 
 export default App;
